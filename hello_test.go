d871f74d47c38a3b43909200b5627740fff34f31
Brent Laster <bclaster@nclasters.org>
2024-10-18 12:06:47 -0400

    Update hello_test.go

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHelloWorld(t *testing.T) {
     expected := "Hello, World!"
-    actual := HelloWorld()
+    actual := HelloWord()
     if actual != expected {
         t.Errorf("Expected %s but got %s", expected, actual)
     }
